13a1cb473c21a59d767abef9b88a359234bec054
xhmueel <105432909+xhmueel@users.noreply.github.com>
2023-12-08 19:56:32 +0000

    feat: Implement children AI

--- project/Child/Script/child.gd
+++ project/Child/Script/child.gd
@@ -1,39 +1,29 @@
 extends CharacterBody3D
 
-
 const SPEED = 5
 const ACCELERATION = 10
-
-#Might need to change the way we have the targets in here
-#@export var target: Area3D
-#@export var door_target: Area3D
-
-
 
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
 
 var caught_gameboy = false
-
-var gameboy
-var door
-
-func _init(GameBoy, Door):
-	gameboy = GameBoy
-	door = Door
+var gameboy = null
+var door = null
 
 func _set_gameboy(GameBoy):
 	gameboy = GameBoy
+	gameboy.body_entered.connect(_on_gameboy_body_entered)
 
 func _set_door(Door):
 	door = Door
+	door.body_entered.connect(_on_door_body_entered)
 	
 func _physics_process(delta):
 	
 	var direction = Vector3()
-	
-	if(caught_gameboy):
+
+	if(door and caught_gameboy):
 		nav.target_position = door.global_position
-	else:
+	elif(door and is_instance_valid(gameboy)):
 		nav.target_position = gameboy.global_position
 
 	direction = nav.get_next_path_position() - global_position
@@ -46,24 +36,11 @@ func _physics_process(delta):
 	
 
 
-#func _on_navigation_agent_3d_target_reached():
-#	if(not caught_gameboy):
-#		get_parent().get_node("Gameboy").queue_free()
-#		caught_gameboy = true
-#	else:
-#		queue_free()
-		
-	
-	
-
-
-
 func _on_gameboy_body_entered(body):
-	caught_gameboy = true
+	if(body == self):
+		caught_gameboy = true
 	
-
 
 func _on_door_body_entered(body):
-	if body.is_in_group("Child"):
-		#queue_free()
-		pass
+	if(body == self):
+		queue_free()

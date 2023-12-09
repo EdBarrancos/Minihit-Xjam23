02aa907c9ca8ae9280e32ce36d6e9fe28ac0186e
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-12-09 16:08:59 +0000

    #17 - Main menu

--- project/Game/Scripts/game.gd
+++ project/Game/Scripts/game.gd
@@ -1,6 +1,17 @@
 extends Node3D
 
+@onready var mainmenu = preload("res://Game/Scenes/main_menu.tscn")
+@onready var level = preload("res://Store/Scenes/store.tscn")
+
+@onready var current_scene = $"Main Menu"
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("EXIT"):
 		get_tree().quit()
+
+
+func loadGame():
+	current_scene.queue_free()
+	current_scene = level.instantiate()
+	add_child(current_scene)
